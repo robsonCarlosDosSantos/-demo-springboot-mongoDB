51cb9d9421e459a06b6f9c42de68958fc22b5bc8
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-02-11 12:29:02 +0000

    Conectando ao MongoDB com repository e service

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -5,24 +5,24 @@ import java.io.Serializable;
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;
+	private String id;
 	private String name;
 	private String email;
 	
 	public User() {}
 
-	public User(Long id, String name, String email) {
+	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,26 +1,28 @@
 package com.example.demo.resources;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User u1 = new User(1L,"Bob Brow", "bob@gmail.com");
-		User u2 = new User(2L,"Maria Green", "maria@gmail.com");
 		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(u1,u2));
+		list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
