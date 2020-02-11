5c3073fbf5af42371942e5cf2ed3acdf29fabf58
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-02-11 17:46:16 +0000

    Criando entity Post com User aninhado

--- src/main/java/com/example/demo/configuration/Config.java
+++ src/main/java/com/example/demo/configuration/Config.java
@@ -1,29 +1,44 @@
 package com.example.demo.configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
+import com.example.demo.repositories.PostRepository;
 import com.example.demo.repositories.UserRepository;
 @Configuration
 public class Config implements CommandLineRunner{
 	
 	@Autowired
-	UserRepository repository;
+	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
-		repository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		repository.saveAll(Arrays.asList(maria,alex,bob));
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Recife, abraços", maria);
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei fez hoje", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(p1,p2));
 		
 	}
 
