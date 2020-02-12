5eb3cb8634cf59569fdb96ae5ca4fb199dfe2c46
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-02-12 15:08:44 +0000

    Projeção dos dados do autor com DTO

--- src/main/java/com/example/demo/configuration/Config.java
+++ src/main/java/com/example/demo/configuration/Config.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
+import com.example.demo.dto.AuthorDTO;
 import com.example.demo.repositories.PostRepository;
 import com.example.demo.repositories.UserRepository;
 @Configuration
@@ -34,10 +35,11 @@ public class Config implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Recife, abraços", maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei fez hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Recife, abraços", new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei fez hoje", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(p1,p2));
 		
 	}
